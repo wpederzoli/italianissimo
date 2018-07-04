a0a4f317570619a90a8fae4c511f5c33a1a23767
William <wpederzoli@gmail.com>
2018-07-04 10:14:24 +0200

    Reviews text added and debug flags removed

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Carousel from 'nuka-carousel'
-import { Star } from '@material-ui/icons'
+import { Star, FormatQuote } from '@material-ui/icons'
 import { getStarsFromRating } from '../helpers'
 
 const Reviews = ({ reviews }) => {
@@ -9,29 +9,35 @@ const Reviews = ({ reviews }) => {
             <div style={{ position: 'relative', flex: 1, backgroundColor: '#000', minHeight: 250 }}>
                 <img alt='reviews' src='../img/reviews.png' style={{ width: '100%', minHeight: 250, opacity: 0.5 }} />
                 <div style={{ width: '100%', position: 'absolute', top: '10%', textAlign: 'center' }}>
-                <h3 style={{ color: '#fff' }}>Algunas reseñas de nuestros clientes</h3>
-                    <Carousel 
-                        autoplay 
-                        swiping 
+                    <h3 style={{ color: '#fff' }}>Algunas reseñas de nuestros clientes</h3>
+                    <Carousel
+                        autoplay
+                        swiping
                         dragging
-                        wrapAround 
+                        wrapAround
                         renderTopCenterControls={() => false}
                         renderCenterCenterControls={() => false}
-                        renderBottomCenterControls={() => false} 
-                        renderCenterLeftControls={() => false} 
+                        renderBottomCenterControls={() => false}
+                        renderCenterLeftControls={() => false}
                         renderCenterRightControls={() => false}
                     >
-                        { reviews.map((review, key) => {
-                            console.log(review)
-                            return(
+                        {reviews.map((review, key) => {
+                            return (
                                 <div>
                                     <h2 style={{ color: '#fff' }}>{review.author_name}</h2>
                                     <div style={{ flexDirection: 'row' }}>
                                         {
-                                            getStarsFromRating(review.rating).map((point) =>{
+                                            getStarsFromRating(review.rating).map((point) => {
                                                 return <Star key={point} style={{ color: '#fff57e' }} />
                                             })
                                         }
+                                    </div>
+                                    <div style={{ flexDirection: 'row' }}>
+                                        <h3 style={{ color: '#fff' }}>
+                                            <FormatQuote style={{ color: '#fff57e' }} />
+                                                {review.text}
+                                            <FormatQuote style={{ color: '#fff57e' }} />
+                                        </h3>
                                     </div>
                                 </div>
                             )
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,11 +1,7 @@
-import React from 'react'
-import { Star } from '@material-ui/icons'
-
 export const getStarsFromRating = (rating) =>{
     let arr = []
     for(let i = 0; i < rating; i++){
         arr.push(i)
     }
-    console.log(arr)
     return arr
 }
