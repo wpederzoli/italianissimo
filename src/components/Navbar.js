07e3a964caa154caedf4a1958d8cd47e4ddb43a2
William <wpederzoli@gmail.com>
2018-07-06 11:21:09 +0200

    Navbar reusable component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,6 @@ import MenuIcon from '@material-ui/icons/Menu'
 
 import { NavLinks } from '.'
 import { navbarEntranceAnimation } from '../animations'
-import { logoImgStyle, menuIconStyle, navbarMenuIconContainerStyle, navlinksContainerStyle, navbarStyle } from '../styles'
 
 class Navbar extends Component {
     constructor(props){
@@ -36,6 +35,8 @@ class Navbar extends Component {
     }
 
     render() {
+        const {navbarStyle, logoImgStyle, navlinksContainerStyle, navbarMenuIconContainerStyle, menuIconStyle } = styles
+        
         return (
             <div>
                 <AppBar id='navbar' style={navbarStyle} >
@@ -54,6 +55,14 @@ class Navbar extends Component {
             </div>
         )
     }
+}
+
+const styles = {
+    navbarStyle: { backgroundColor: '#f7f7f7' },
+    logoImgStyle: { width: 100, height: 100, margin: 5 },
+    navlinksContainerStyle: { flex: 1, marginRight: 20 },
+    navbarMenuIconContainerStyle: { float: 'right' },
+    menuIconStyle: { fontSize: 40 }
 }
 
 export { Navbar }
