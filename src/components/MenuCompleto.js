0f52e8919b5fb514eb832765c65c795462488aa6
William <wpederzoli@gmail.com>
2018-07-06 14:16:25 +0200

    Menu automated and bugs fixed

--- src/components/MenuCompleto.js
+++ src/components/MenuCompleto.js
@@ -3,26 +3,48 @@ import { Grid } from '@material-ui/core'
 
 const MenuCompleto = ({ categories, fullMenu }) => {
 
-    const { menuContainerStyle, backgroundImageStyle, menuPageContainerStyle, categoryTitle, categoryTitleContainer } = styles
+    const {
+        menuContainerStyle,
+        menuPageContainerStyle,
+        categoryTitle,
+        categoryTitleContainer,
+        menuItemTitle,
+        menuItemDescription,
+        menuItemPrice
+    } = styles
 
     return (
         <div style={menuContainerStyle}>
-            <div style={menuPageContainerStyle}>
-            {/* <img alt='fondo-menu' src='./img/fondo-menu.png' style={backgroundImageStyle} /> */}
-                {
-                    categories.map((category, key) =>{
-                        return(
-                            <Grid container>
-                                <Grid item lg={4} md={4} xs={4} />
-                                <Grid item lg={4} md={4} xs={4} style={categoryTitleContainer}>
-                                    <p style={categoryTitle}>{category.toUpperCase()}</p>
+            {
+                categories.map((category, key) => {
+                    return (
+                        <div style={menuPageContainerStyle}>
+                            <div key={key} style={{ padding: '5%' }}>
+                                <Grid container>
+                                    <Grid item lg={4} md={4} xs={4} />
+                                    <Grid item lg={4} md={4} xs={4} style={categoryTitleContainer}>
+                                        <p style={categoryTitle}>{category.toUpperCase()}</p>
+                                    </Grid>
+                                    <Grid item lg={4} md={4} xs={4} />
                                 </Grid>
-                                <Grid item lg={4} md={4} xs={4} />
-                            </Grid>
-                        )
-                    })
-                }
-            </div>
+                                <Grid container>
+                                    {
+                                        fullMenu[category].map((item, key) => {
+                                            return (
+                                                <Grid key={key} item lg={6} md={6} xs={12}>
+                                                    <p style={menuItemTitle}>{item.name.toUpperCase()}</p>
+                                                    <p style={menuItemDescription}>{item.description}</p>
+                                                    <p style={menuItemPrice}>${item.price}</p>
+                                                </Grid>
+                                            )
+                                        })
+                                    }
+                                </Grid>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
@@ -49,10 +71,23 @@ const styles = {
         backgroundSize: '100% 100%',
         backgroundRepeat: 'no-repeat'
     },
-    categoryTitle: { 
-        color: '#fff', 
-        textAlign:'center', 
-        fontWeight:'600' 
+    categoryTitle: {
+        color: '#fff',
+        textAlign: 'center',
+        fontWeight: '600'
+    },
+    menuItemTitle: {
+        textAlign: 'center',
+        color: 'grey',
+        fontWeight: '500'
+    },
+    menuItemDescription: {
+        textAlign: 'center'
+    },
+    menuItemPrice: {
+        textAlign: 'center',
+        color: 'darkgrey',
+        fontWeight: '600'
     }
 }
 
